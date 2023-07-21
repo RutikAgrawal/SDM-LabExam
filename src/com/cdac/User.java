64936f38b7598160f58dfcfcf7bd0c9ca5059c8d
Rutik <ritikagarwal078gmail.com>
2023-07-21 17:11:32 +0530

    my second commit after adding date and time

--- src/com/cdac/User.java
+++ src/com/cdac/User.java
@@ -1,5 +1,7 @@
 package com.cdac;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class User {
@@ -10,6 +12,13 @@ public class User {
 	        System.out.print("Enter your name: ");
 	        String name = scanner.nextLine();
 	        System.out.println("Welcome, " + name + " to my repository!");
+	        
+	        LocalDateTime now = LocalDateTime.now();
+	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	        String formattedDateTime = now.format(formatter);
+
+	        // b) Display the date and time along with the welcome message
+	        System.out.println("Current Date and Time: " + formattedDateTime);
 	}
 
 }
